e2ebcc0855e4937b7b7b5e374403160535c45c14
devopst2020 <satyareddyipl@gmail.com>
2018-09-17 18:42:45 +0100

    adding db

--- routes/trains.js
+++ routes/trains.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var low = require('lowdb')
-var FileSync = require('lowdb/adapters/FileSync')
+//var low = require('lowdb')
+//var FileSync = require('lowdb/adapters/FileSync')
 
-var adapter = new FileSync('data/trains.json')
-var db = low(adapter)
+//var adapter = new FileSync('data/trains.json')
+//var db = low(adapter)
 
+var MongoClient = require('mongodb').MongoClient;
 /* GET trains listing. */
 router.get('/', function(req, res, next) {
-  res.send(db.get('trains').sortBy('name').value());
+//  res.send(db.get('trains').sortBy('name').value());
+
+  // Connect to the db
+MongoClient.connect("mongodb://localhost:27017/trainsdb", function (err, db) {
+    
+  db.collection('trains', function (err, collection) {      
+ collection.find().toArray(function(err, items) {
+  if(err) throw err;
+  res.send(items[0].trains);
+});
+      
+  });                
+});
 });
 
 module.exports = router;
